2d8fe327d2bd9e6243e18b98abbe2423f44c79bd
Taylor Lo <taylorloph@gmail.com>
2023-10-29 22:51:25 -0400

    Create SalesPerson class

---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent {
+
+}
