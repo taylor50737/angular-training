adba9ecfbc59f1bb75f4ae4e180ba762d56ea28f
Taylor Lo <taylorloph@gmail.com>
2023-10-29 23:11:06 -0400

    Added bootstrap and table css

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,11 +3,11 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
-  
+
   salesPersonList: SalesPerson[] = [
     new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code", 50000),
     new SalesPerson("John", "Doe", "john.doe@luv2code", 40000),
