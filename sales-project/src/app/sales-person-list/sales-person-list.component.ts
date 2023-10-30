310c6e2557efe51867aa99f8eb7b0b328e0aaa05
Taylor Lo <taylorloph@gmail.com>
2023-10-29 22:56:34 -0400

    Added SalesPerson data

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
-export class SalesPersonListComponent {
+export class SalesPersonListComponent implements OnInit {
+  
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code", 50000),
+    new SalesPerson("John", "Doe", "john.doe@luv2code", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@luv2code", 60000)
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 }
